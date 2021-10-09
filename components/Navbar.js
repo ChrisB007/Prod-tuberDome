4c11364d44382032fa5da2df897a2d342ba61d08
ChrisB007 <chrisbitoy@gmail.com>
2021-10-09 12:41:26 -0400

    login button updated

--- components/Navbar.js
+++ components/Navbar.js
@@ -352,7 +352,7 @@ export default function Navbar() {
               {!session && (
                 <a
                   href={`/api/auth/signin`}
-                  className="text-base font-medium text-gray-500 hover:text-gray-900"
+                  className="ml-8 inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
                   onClick={(e) => signIn()}
                 >
                   Sign in
@@ -364,7 +364,7 @@ export default function Navbar() {
                   className="ml-8 inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
                   onClick={(e) => signOut}
                 >
-                  Sign up
+                  Sign Out
                 </a>
               )}
             </div>
