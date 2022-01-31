6fc675e643ebe3c5bdbf7acec27cf9c7fb8b58c1
ChrisB007 <chrisbitoy@gmail.com>
2022-01-31 12:14:53 -0500

    updated callall route

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,31 +1,24 @@
 /* This example requires Tailwind CSS v2.0+ */
-import { Fragment } from 'react';
-import { Popover, Transition } from '@headlessui/react';
-import { MenuIcon, XIcon } from '@heroicons/react/outline';
-import { signIn, signOut, getSession } from 'next-auth/react';
-import Link from 'next/link';
+import { Popover, Transition } from "@headlessui/react";
+import { MenuIcon, XIcon } from "@heroicons/react/outline";
+import Link from "next/link";
+import { getSession, signIn, signOut } from "next-auth/react";
+import { Fragment } from "react";
 
-function classNames(...classes) {
-  return classes.filter(Boolean).join(' ');
-}
+//function classNames(...classes) {
+//  return classes.filter(Boolean).join(" ");
+//}
 
 export default function Navbar({ session }) {
   return (
     <Popover className="relative bg-white">
-      <div
-        className="absolute inset-0 shadow z-30 pointer-events-none"
-        aria-hidden="true"
-      />
+      <div className="absolute inset-0 shadow z-30 pointer-events-none" aria-hidden="true" />
       <div className="relative z-20">
         <div className="max-w-7xl mx-auto flex justify-between items-center px-4 py-5 sm:px-6 sm:py-4 lg:px-8 md:justify-start md:space-x-10">
           <div>
-            <a href={session ? '/protected' : '/'} className="flex">
+            <a href={session ? "/protected" : "/"} className="flex">
               <span className="sr-only">TuberDome</span>
-              <img
-                className="h-8 w-auto sm:h-10"
-                src="/images/berdome.png"
-                alt="logo"
-              />
+              <img className="h-8 w-auto sm:h-10" src="/images/berdome.png" alt="logo" />
             </a>
           </div>
           <div className="-mr-2 -my-2 md:hidden">
@@ -43,30 +36,24 @@ export default function Navbar({ session }) {
               </a>
               <a
                 href="/creators"
-                className="text-base font-medium text-gray-500 hover:text-gray-900"
-              >
+                className="text-base font-medium text-gray-500 hover:text-gray-900">
                 Creators
               </a>
-              <a
-                href="/invest"
-                className="text-base font-medium text-gray-500 hover:text-gray-900"
-              >
+              <a href="/invest" className="text-base font-medium text-gray-500 hover:text-gray-900">
                 Invest in Us
               </a>
               <a
                 href="/contact"
-                className="text-base font-medium text-gray-500 hover:text-gray-900"
-              >
+                className="text-base font-medium text-gray-500 hover:text-gray-900">
                 Contact Us
               </a>
             </Popover.Group>
             <div className="flex items-center md:ml-12">
               {!session && (
                 <Link
-                  href={`/login`}
+                  href={`/api/auth/signin`}
                   className="ml-8 inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-gray-600 hover:bg-gray-700"
-                  onClick={(e) => signIn()}
-                >
+                  onClick={() => signIn()}>
                   <a>Sign In</a>
                 </Link>
               )}
@@ -74,8 +61,7 @@ export default function Navbar({ session }) {
                 <Link
                   href={`/api/auth/signout`}
                   className="ml-8 inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-gray-600 hover:bg-gray-700"
-                  onClick={(e) => signOut()}
-                >
+                  onClick={() => signOut()}>
                   <a>Sign Out</a>
                 </Link>
               )}
@@ -91,21 +77,15 @@ export default function Navbar({ session }) {
         enterTo="opacity-100 scale-100"
         leave="duration-100 ease-in"
         leaveFrom="opacity-100 scale-100"
-        leaveTo="opacity-0 scale-95"
-      >
+        leaveTo="opacity-0 scale-95">
         <Popover.Panel
           focus
-          className="absolute z-30 top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden"
-        >
+          className="absolute z-30 top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden">
           <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-white divide-y-2 divide-gray-50">
             <div className="pt-5 pb-6 px-5 sm:pb-8">
               <div className="flex items-center justify-between">
                 <div>
-                  <img
-                    className="h-8 w-auto"
-                    src="/images/berdome.png"
-                    alt="TuberDome"
-                  />
+                  <img className="h-8 w-auto" src="/images/berdome.png" alt="TuberDome" />
                 </div>
                 <div className="-mr-2">
                   <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-gray-500">
@@ -119,27 +99,23 @@ export default function Navbar({ session }) {
                   <div className="grid grid-cols-2 gap-4">
                     <a
                       href="/sponsors"
-                      className="rounded-md text-base font-medium text-gray-900 hover:text-gray-700"
-                    >
+                      className="rounded-md text-base font-medium text-gray-900 hover:text-gray-700">
                       Sponsors
                     </a>
 
                     <a
                       href="/creators"
-                      className="rounded-md text-base font-medium text-gray-900 hover:text-gray-700"
-                    >
+                      className="rounded-md text-base font-medium text-gray-900 hover:text-gray-700">
                       Creators
                     </a>
                     <a
                       href="/invest"
-                      className="text-base font-medium text-gray-500 hover:text-gray-900"
-                    >
+                      className="text-base font-medium text-gray-500 hover:text-gray-900">
                       Invest in Us
                     </a>
                     <a
                       href="/contact"
-                      className="rounded-md text-base font-medium text-gray-900 hover:text-gray-700"
-                    >
+                      className="rounded-md text-base font-medium text-gray-900 hover:text-gray-700">
                       Contact Us
                     </a>
                   </div>
@@ -150,21 +126,19 @@ export default function Navbar({ session }) {
               <div className="mt-6">
                 {!session && (
                   <Link
-                    href={`/login`}
-                    onClick={(e) => signIn()}
-                    className="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-gray-600 hover:bg-gray-700"
-                  >
+                    href={`/api/auth/signin`}
+                    onClick={() => signIn()}
+                    className="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-gray-600 hover:bg-gray-700">
                     <a>Sign In</a>
                   </Link>
                 )}
                 {session && (
                   <p className="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-gray-600 hover:bg-gray-700">
-                    Existing customer?{' '}
+                    Existing customer?{" "}
                     <Link
                       href={`/api/auth/signout`}
-                      onClick={(e) => signOut()}
-                      className="text-gray-600 hover:text-gray-500"
-                    >
+                      onClick={() => signOut()}
+                      className="text-gray-600 hover:text-gray-500">
                       <a>Sign Out</a>
                     </Link>
                   </p>
@@ -178,18 +152,18 @@ export default function Navbar({ session }) {
   );
 }
 
-export async function getServerSideProps({ req, res }) {
+export async function getServerSideProps({ req }) {
   const session = await getSession({ req });
 
   if (!session) {
     return {
       redirect: {
-        destination: '/',
+        destination: "/",
         permanent: false,
       },
     };
   }
-  console.log('Sess', session);
+  console.log("Sess", session);
 
   return {
     props: {
--- pages/login.tsx
+++ pages/login.tsx
@@ -104,6 +104,7 @@ const Login: FC = () => {
                           id="email"
                           name="email"
                           type="email"
+                          placeholder="Get login link sent to your email"
                           autoComplete="email"
                           required
                           className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-red-500 focus:border-red-500 sm:text-sm"
