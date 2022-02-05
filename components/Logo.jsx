b58b4c50e32e86dd3527c4f73419eecdff28e3bc
ChrisB007 <chrisbitoy@gmail.com>
2022-02-05 02:41:19 -0500

    removed nextAuth refs

--- components/Logo.jsx
+++ components/Logo.jsx
@@ -1,12 +1,10 @@
-import React from 'react';
-import Link from 'next/link';
-import { session, useSession } from 'next-auth/react';
+import Link from "next/link";
+import React from "react";
 
 function Logo() {
-  const user = session.user;
   return (
     <div className="inline-flex h-5">
-      <Link href={user ? '/dashboard' : '/'}>
+      <Link href={""}>
         <div className="cursor-pointer inline-flex ">
           <>
             <span className="sr-only">TuberDome</span>
@@ -19,10 +17,9 @@ function Logo() {
                 stroke="currentColor"
                 strokeWidth="2"
                 strokeLinecap="round"
-                strokeLinejoin="round"
-              >
-                {' '}
-                <path d="M22.54 6.42a2.78 2.78 0 0 0-1.94-2C18.88 4 12 4 12 4s-6.88 0-8.6.46a2.78 2.78 0 0 0-1.94 2A29 29 0 0 0 1 11.75a29 29 0 0 0 .46 5.33A2.78 2.78 0 0 0 3.4 19c1.72.46 8.6.46 8.6.46s6.88 0 8.6-.46a2.78 2.78 0 0 0 1.94-2 29 29 0 0 0 .46-5.25 29 29 0 0 0-.46-5.33z" />{' '}
+                strokeLinejoin="round">
+                {" "}
+                <path d="M22.54 6.42a2.78 2.78 0 0 0-1.94-2C18.88 4 12 4 12 4s-6.88 0-8.6.46a2.78 2.78 0 0 0-1.94 2A29 29 0 0 0 1 11.75a29 29 0 0 0 .46 5.33A2.78 2.78 0 0 0 3.4 19c1.72.46 8.6.46 8.6.46s6.88 0 8.6-.46a2.78 2.78 0 0 0 1.94-2 29 29 0 0 0 .46-5.25 29 29 0 0 0-.46-5.33z" />{" "}
                 <polygon points="9.75 15.02 15.5 11.75 9.75 8.48 9.75 15.02" />
               </svg>
 
